be9e33c14181f5a92bae1f1295057dbd837c7eb9
Melissa <melissaguillen70@yahoo.com>
2023-07-31 12:06:51 -0700

    Puppy Data with useState

--- PuppyPals/src/App.jsx
+++ PuppyPals/src/App.jsx
@@ -1,9 +1,20 @@
+import {puppyList} from './data.js'
+import { useState } from 'react'
 import './App.css'
 
 function App() {
-  
-}
-      <div>
-      </div>
+  const [puppies, setPuppies] = useState(puppyList)
 
+console.log("puppyList: ", puppyList)
+
+  return (
+    <div className="App">
+      {
+        puppies.map((puppy) => {
+          return <p key={puppy.id}>{puppy.name}</p>
+        })
+      }
+    </div>
+  )
+}
 export default App
